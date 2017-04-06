cebab3a28a3fc6318c5ac296fae4bce16e059f6e
Christopher Yammine <christophery@mogo.ca>
2017-04-06 11:33:39 -0700

    Initial commit: - Most of raw mode implemented

---
+++ kilo.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (isprint(c)) {
+      printf("%d ('%c')\r\n", c, c);
+    } else {
+      printf("%d\r\n", c);
+    }
+  }
+  return 0;
+}
