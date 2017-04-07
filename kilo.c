362109ec019bacb06a30175b089aac16f71f8275
Chris Yammine <me@chrisyammine.com>
2017-04-06 23:35:42 -0700

    Add del key but dont make it do anything for now

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -85,6 +86,7 @@ int editorReadKey() {
           switch (seq[1]) {
             case '1': return HOME_KEY;
             case '2': return END_KEY;
+            case '3': return DEL_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
             case '7': return HOME_KEY;
